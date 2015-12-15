3d4cec99fda9330acadbf2b8370743b61d42b257
Mike Reich <mjreich@gmail.com>
2015-12-15 09:51:07 -0800

    Supporting callback handlers as well as file templates

--- src/Templater.js
+++ src/Templater.js
@@ -1,7 +1,7 @@
 /* 
 * @Author: Mike Reich
 * @Date:   2015-07-22 09:45:05
-* @Last Modified 2015-12-14
+* @Last Modified 2015-12-15
 */
 
 'use strict';
@@ -20,13 +20,21 @@ export default class Templater {
 
   }
 
-  _register(name, type, filename) {
-    this._templates[name] = {type, filename}
+  _register(name, type, handler) {
+    this._templates[name] = {type, handler}
   }
 
   _render(name, args) {
     if(!this._templates[name]) throw new Error('Template name '+name+' not found')
     var opts = this._templates[name]
-    return this.app.get('renderer').request('renderFile', opts.filename, args);
+    if(typeof opts.handler === 'string') {
+      args.filename = opts.handler
+      return this.app.get('renderer').request('renderFile', opts.handler, args);
+    } else { //assume its a callable returning a promise
+      return opts.handler(name, args).then((template) => {
+        console.log('template returned', template)
+        return this.app.get('renderer').request('render', opts.type, template, args)
+      })
+    }
   }
 }
