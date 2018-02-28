45e1c2f1dbbd3563722770c57007c77860e3b10c
Luke Opperman <luke@opperman.net>
2018-02-28 12:10:52 -0500

    Don't even need to check for _renderedPartials

--- src/modules/templater-ejs/index.js
+++ src/modules/templater-ejs/index.js
@@ -18,15 +18,15 @@ class TemplaterEjs extends NxusModule {
   _locals([type, content, opts]) {
     if (opts) {
       // {id: inner promise} for all renders inside the current template
-      if (!opts._renderedPartials) {
-        opts._renderedPartials = {}
-      }
+      opts._renderedPartials = {}
+
       opts.render = (name, newOpts) => {
         let id = uuid.v4()
         // new levels should get their own _renderedPartials and render
         if (!newOpts) {
           newOpts = _.omit(opts, "_renderedPartials", "render")
         }
+        // TODO this probably is never used
         newOpts._inlineRenderId = id
         // save the promise for this render in the parent opts
         opts._renderedPartials[id] = templater.render(name, newOpts).catch((e) => {
@@ -43,6 +43,7 @@ class TemplaterEjs extends NxusModule {
     if (opts._renderedPartials && _.isString(result)) {
       // replace the id placeholders in this template with the results of the renderedPartials promises
       return Promise.mapSeries(Object.keys(opts._renderedPartials), (id) => {
+        // TODO this probably is never used
         if(opts._inlineRenderId == id) return Promise.resolve(result)
         return opts._renderedPartials[id].then((part) => {
           result = result.replace('<<<'+id+'>>>', part)
